62671564584e6fe43abcbcfde93a896555fbc4d8
Kamal Nayan tiwari <kamalnayan1148@gmail.com>
2026-08-20 11:20:54 +0530

    Demo

--- my-resume/src/App.jsx
+++ my-resume/src/App.jsx
@@ -1,14 +1,13 @@
-
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import Demo from './components/Demo'
 
 function App() {
   
 
   return(
-    <h1>KAMAL</h1>
+    <>
+    <Demo/>
+    </>
   )
 }
 export default App()
---
+++ my-resume/src/assets/components/Demo.jsx
@@ -0,0 +1,9 @@
+function Demo(){
+return(
+<>
+<h1>KAMAL TIWARI</h1>
+<P>Lorem ipsum dolor sit amet.</P>
+</>
+);
+}
+export default Demo;
